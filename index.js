24394a862a4b817cef105d20be0e9b91e4292c15
sumn2u <sumn2u@gmail.com>
2024-08-13 09:27:24 -0500

    make title and button text configurable

--- index.js
+++ index.js
@@ -15,13 +15,18 @@ module.exports = {
     blocks: {
         aceeditor: {
             process: function(block) {
+                // Extract parameters with default values
+                const kwargs = block.kwargs || {};
+                const compilerTitle = kwargs.compilerTitle || "Try Yourself!";
+                const compilerOutputTitle = kwargs.compilerOutputTitle || "Output:";
+                
                 // HTML content for the Ace Editor
                 return `
                     <div class="compiler-container">
-                        <h2 class="compiler-title">Try Yourself!</h2>
+                        <h2 class="compiler-title">${compilerTitle}</h2>
                         <div id="editor" class="compiler-textarea" style="height: 200px; width: 100%;"></div>
                         <button id="run" class="compiler-button">Run Code</button>
-                        <h3 class="compiler-output-title">Output:</h3>
+                        <h3 class="compiler-output-title">${compilerOutputTitle}</h3>
                         <pre id="compiler-output" class="compiler-output"></pre>
                     </div>
                 `;
