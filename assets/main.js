91be8c8a242a539396d409881672f75c70fb8ca6
THEBOSS0369 <anujkumsharma9876@gmail.com>
2024-08-16 11:58:17 +0530

    Added autocompletion

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-require(["gitbook"], function(gitbook, $) {
+require(["gitbook"], function (gitbook, $) {
 
     function loadAceEditor() {
         // Initialize Ace Editor
@@ -8,26 +8,34 @@ require(["gitbook"], function(gitbook, $) {
             editor.setTheme("ace/theme/tomorrow");
             editor.session.setMode("ace/mode/javascript");
             editor.setValue("// Write your JavaScript code here");
-    
+
             // Disable worker.js
             if (editor && editor.session) {
                 editor.session.setOption("useWorker", false);
             }
-            
+
+            // Auto Completion For the Js interpreter
+            ace.require("ace/ext/langauge_tools");
+            editor.setOptions({
+                enableBasicAutoCompletion: true,
+                enableLiveAutoCompletion: true,
+                enableSnippets: true,
+            })
+
             // Handle the run button click event
             document.getElementById("run").addEventListener("click", function () {
                 // Get the code from the editor
                 var code = editor.getValue();
-    
+
                 // Reset the output area
                 var outputElement = document.getElementById("compiler-output");
                 outputElement.textContent = "";
-    
+
                 // Store the original console.log function
                 var originalConsoleLog = console.log;
-    
+
                 // Override console.log to display output in the designated area
-                console.log = function(...messages) {
+                console.log = function (...messages) {
                     // Format each message
                     var formattedMessages = messages.map(message => {
                         if (typeof message === "object") {
@@ -39,7 +47,7 @@ require(["gitbook"], function(gitbook, $) {
                     // Join messages and append to the output area
                     outputElement.textContent += formattedMessages.join(" ") + "\n";
                 }
-    
+
                 try {
                     // Execute the user's code
                     eval(code);
@@ -47,19 +55,18 @@ require(["gitbook"], function(gitbook, $) {
                     // Display error message in the output area
                     console.log("Error: " + error.message);
                 }
-    
+
                 // Restore the original console.log function
                 console.log = originalConsoleLog;
             });
         }
     }
-    window.onload = function() {
+    window.onload = function () {
         loadAceEditor();
         // When GitBook is ready
-        gitbook.events.bind("page.change", function() {
+        gitbook.events.bind("page.change", function () {
             loadAceEditor();
-            
+
         });
-      }
-    });
-    
+    }
+});
